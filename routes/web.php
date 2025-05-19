3c8446d0882e9dbfdb96815a358044880de521d9
Sourov Biswas <sourov2305101004@diu.edu.bd>
2025-05-19 19:15:32 +0600

    remove unused routes for events and about pages from web.php

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,4 @@ use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
-Route::get('/asd', [PageController::class, 'home'])->name('events');
-Route::get('/asdasd', [PageController::class, 'home'])->name('about');
 
