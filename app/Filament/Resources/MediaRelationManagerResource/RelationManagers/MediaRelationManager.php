ff490cd239d6fb36014c1e1ea1f789fa46fa3a49
Sourov Biswas <sourov2305101004@diu.edu.bd>
2025-05-19 11:46:14 +0600

    add MediaRelationManager and enhance GalleryResource; improve gallery management with new media handling and refined form structure

--- app/Filament/Resources/MediaRelationManagerResource/RelationManagers/MediaRelationManager.php
+++ app/Filament/Resources/MediaRelationManagerResource/RelationManagers/MediaRelationManager.php
@@ -7,8 +7,6 @@ use Filament\Forms\Form;
 use Filament\Resources\RelationManagers\RelationManager;
 use Filament\Tables;
 use Filament\Tables\Table;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
 use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
 class MediaRelationManager extends RelationManager
