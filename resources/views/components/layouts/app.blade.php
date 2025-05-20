60c61010da7a465a1a6c5c7f43a6ab88e21a3cd7
Sourov Biswas <sourov2305101004@diu.edu.bd>
2025-05-20 14:07:34 +0600

    fix: Change HTML class from light to dark for improved theme consistency

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,7 +1,7 @@
 @props(['title'])
 
     <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="light">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="dark">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
