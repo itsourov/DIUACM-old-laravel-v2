f9d3c05bb2f247a3fd87bc41158087bebbfc69cd
Sourov Biswas <sourov2305101004@diu.edu.bd>
2025-05-25 15:13:38 +0600

    feat: Add permission management with policies and migration setup

--- app/Models/User.php
+++ app/Models/User.php
@@ -13,11 +13,13 @@ use Spatie\Image\Enums\Fit;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
 use Spatie\MediaLibrary\MediaCollections\Models\Media;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable implements HasMedia
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
+    use HasRoles;
 
     use InteractsWithMedia;
 
