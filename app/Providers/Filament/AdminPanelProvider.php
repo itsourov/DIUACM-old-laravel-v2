7de5022a9c250047f8c6b6559d80fc13efac55bf
Sourov Biswas <sourov2305101004@diu.edu.bd>
2025-05-26 01:14:25 +0600

    feat: Implement role management with create, edit, and view functionalities

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -27,8 +27,8 @@ class AdminPanelProvider extends PanelProvider
             ->default()
             ->id('admin')
             ->path('admin')
-            ->login()
-            ->registration()
+//            ->login()
+//            ->registration()
             ->colors([
                 'primary' => Color::Blue,
             ])
